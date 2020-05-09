68f7cb14eb4471845a74c87bdc2cc9845b697de3
Nathan André <nathan_99_@hotmail.com>
2020-05-09 14:53:54 -0300

    update avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,25 +1,40 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // usersRouter.get('/', (request, response) => {});
-usersRouter.post('/', async (request, response) => {
-	try {
-		const { name, email, password } = request.body;
-		const createUser = new CreateUserService();
-		const user = await createUser.execute({
-			name,
-			email,
-			password,
+usersRouter.post('/', async (request: Request, response: Response) => {
+	const { name, email, password } = request.body;
+	const createUser = new CreateUserService();
+	const user = await createUser.execute({
+		name,
+		email,
+		password,
+	});
+
+	delete user.password;
+
+	return response.json(user);
+});
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('file'),
+	async (request: Request, response: Response) => {
+		const updateUserAvatarService = new UpdateUserAvatarService();
+		const user = await updateUserAvatarService.execute({
+			userId: request.user.id,
+			avatarFilename: request.file.filename,
 		});
 
-		delete user.password;
-
 		return response.json(user);
-	} catch (err) {
-		return response.status(400).json(err.message);
-	}
-});
+	},
+);
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import 'express-async-errors';
 import routes from './routes';
 import './database';
 import AppError from './errors/AppError';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
